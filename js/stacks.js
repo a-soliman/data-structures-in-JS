f72921663186b16defc62bf720dd1b3640984551
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-01-13 10:02:08 -0800

    coded peek method

--- js/stacks.js
+++ js/stacks.js
@@ -12,3 +12,9 @@
 // stack.prototype.pop = function() {
 //   return this.items.pop();
 // };
+
+// //peek method
+// stack.prototype.peek = function() {
+//   var index = this.items.length -1;
+//   return this.items[index];
+// };
