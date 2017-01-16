b7a7f4af4609cbe97dea6d69f04c0f1d27260ae1
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-01-16 10:30:09 -0800

    coded size method through the prototype

--- js/linkedlist.js
+++ js/linkedlist.js
@@ -10,3 +10,28 @@ function Node(value, next, prev) {
 	this.next = next;
 	this.prev = prev;
 }
+
+// isEmpty to check if the LinkedList is EMPTY
+LinkedList.prototype.isEmpty = function() {
+  if(this.head) {
+    return false;
+  }
+  return true;
+};
+
+// size method o return the lengeth if the list 
+LinkedList.prototype.size = function() {
+  if(!this.head) {
+    return null;
+  }
+  else {
+    var currentNode = this.head;
+    var size = 0;
+    
+    while(currentNode) {
+      currentNode = currentNode.next;
+      size++;
+    }
+  }
+  return size;
+};
