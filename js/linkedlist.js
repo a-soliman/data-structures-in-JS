30ceda1b928aec8cc54d18b84a989ade13ce2c01
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-01-16 10:28:46 -0800

    coded a node cons func

--- js/linkedlist.js
+++ js/linkedlist.js
@@ -3,3 +3,10 @@ function LinkedList() {
 	this.head = null;
 	this.tail = null;
 }
+
+// Node constructor function
+function Node(value, next, prev) {
+	this.value = value;
+	this.next = next;
+	this.prev = prev;
+}
