e6a461b404a522a4c63df051a4aaf902ba173889
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-01-19 10:44:25 -0800

    coded a prototype add method

--- js/sets.js
+++ js/sets.js
@@ -2,3 +2,12 @@
 function Set() {
   this.items = {};
 }
+
+// add method to add 
+Set.prototype.add = function(value) {
+  if(!this.has(value)) {
+    this.items[value] = value;
+    return true;
+  }
+  return false;
+};
