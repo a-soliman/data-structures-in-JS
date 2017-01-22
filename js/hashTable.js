20c5bd186a71823fa70a904a48eefbb22d01e8f3
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-01-22 11:00:38 -0800

    coded a hash() method to hash a letter into a number

--- js/hashTable.js
+++ js/hashTable.js
@@ -10,3 +10,14 @@ function HashNode(key, value, next) {
   this.value = value;
   this.next = next || null;
 }
+
+// hash method => hashes a letter into Number
+HashTable.prototype.hash = function(key) {
+  var total = 0;
+  for(var i = 0; i < key.length; i++) {
+    total += key.charCodeAt(i);
+  }
+  var bucket = total % this.numBuckets;
+  
+  return bucket;
+}
