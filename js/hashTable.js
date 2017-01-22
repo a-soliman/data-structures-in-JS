826db4bac2e73bfe66839521d4a9d9aba0ff670c
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-01-22 10:59:49 -0800

    coded a node cons function

--- js/hashTable.js
+++ js/hashTable.js
@@ -3,3 +3,10 @@ function HashTable(size) {
   this.buckets = Array(size);
   this.numBuckets = this.buckets.length;
 }
+
+//Nodes function
+function HashNode(key, value, next) {
+  this.key = key;
+  this.value = value;
+  this.next = next || null;
+}
