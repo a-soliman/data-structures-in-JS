93347779a606d50c99a74a036c7ec1cecda8b8e5
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-01-22 11:02:17 -0800

    coded an insert method

--- js/hashTable.js
+++ js/hashTable.js
@@ -37,3 +37,35 @@ HashTable.prototype.hash = function(key) {
 //     currentNode.next = new HashNode(key, value);
 //   }
 // }
+
+
+// insert method to insert a new bucket or change the value of an existing one
+HashTable.prototype.insert = function(key, value) {
+  //hash the key to number so the bucket gets desided
+  var index = this.hash(key);
+  
+  //if there the bucket is free then place it there
+  if(!this.buckets[index]) {
+    this.buckets[index] = new HashNode(key, value);
+  }
+  // check if the first position is equal to the new node's key => if yes then change the value
+  else if(this.buckets[index].key === key) {
+    this.buckets[index].value = value;
+  }
+  //if non of the above
+  else {
+    var currentNode = this.buckets[index];
+    
+    while(currentNode.next) {
+      //check if any of the existing nodes in the ckain have the same key => if yes then change the value
+      if(currentNode.next.key === key) {
+        currentNode.next.value = value;
+        return;
+      }
+      //if not then contenu looping
+      currentNode = currentNode.next;
+    }
+    //if you didnt find the placethe new node at the very end of the chain inside the bucket
+    currentNode.next = new HashNode(key, value)
+  }
+}
