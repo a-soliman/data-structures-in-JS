83f135d301e0a1253c2ecf734645a6437f80311f
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-01-23 11:08:50 -0800

    coded a delete method

--- js/dictionary.js
+++ js/dictionary.js
@@ -6,3 +6,12 @@ function Dictionary() {
 Dictionary.prototype.set = function(key, value) {
   this.items[key] = value;
 };
+
+// delelte method
+Dictionary.prototype.delete = function(key) {
+  if(this.has(key)) {
+    delete this.items[key];
+    return true;
+  }
+  return false;
+};
