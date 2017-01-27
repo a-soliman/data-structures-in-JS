6c2ef14624c6956fd16ce3a5c06c9f185fd52874
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-01-27 10:08:51 -0800

    coded an insert method

--- js/bst.js
+++ js/bst.js
@@ -5,3 +5,17 @@ function BST(value) {
   this.left = null;
   this.right = null;
 }
+// INSERT method
+
+BST.prototype.insert = function(value) {
+  //chck if the value is less than or equal to the prototype
+  if(value <= this.value) {
+    //check if the LEFT side is free
+    if(!this.left) {
+      this.left = new BST(value);
+    }
+    //Otherwise
+    else {
+      this.left.insert(value)
+    }
+  }
