ffc5c71dd4c00a9116c5bb8b323ee11ee3fb9ac9
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-01-28 14:13:48 -0800

    testing the binary search tree

--- js/bst.js
+++ js/bst.js
@@ -179,3 +179,18 @@ BST.prototype.find = function(value) {
 function log(value) {
 	console.log(value)
 }
+
+// === Test ===
+var bst = new BST(50);
+
+
+bst.insert(30);
+bst.insert(70);
+bst.insert(100);
+bst.insert(60);
+bst.insert(59);
+bst.insert(45);
+bst.insert(35);
+bst.insert(85);
+bst.insert(105);
+bst.insert(10);
