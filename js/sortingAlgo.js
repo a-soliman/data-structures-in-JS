4ffa5f602f3b52d99745cf59cdea9ef5114580c8
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-01-29 13:34:30 -0800

    coded the bubble sort method

--- js/sortingAlgo.js
+++ js/sortingAlgo.js
@@ -13,3 +13,16 @@ function ArrayList() {
 
   //The bubble sort Algo
   this.bubbleSort = function() {
+
+  	//store the size of the array
+    var length = array.length;
+    //loop through the array
+    for(var i = 0; i <length; i++ ) {
+      //loop once again using -1
+      for(var j = 0; j <length -1; j++) {
+        if(array[j] > array[j+1]) {
+          swap(array, j, j+1);
+        }
+      }
+    }
+  };
