e886dba83de2e100120f74ac44d3d5d5c9d4fc7c
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-01-30 13:36:35 -0800

    coded the selection sort method

--- js/sortingAlgo.js
+++ js/sortingAlgo.js
@@ -39,3 +39,16 @@ function ArrayList() {
     
     var length = array.length;
     var indexMin;
+    for(var i = 0; i < length-1; i++) {
+      indexMin = i;
+      
+      for(var j = i; j <length; j++) {
+        if(array[indexMin] > array[j]) {
+          indexMin = j;
+        }
+      }
+      if(i !== indexMin) {
+        swap(array, i, indexMin);
+      }
+    }
+  };
