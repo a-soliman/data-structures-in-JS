aad8a91b48e8960fe2a33d7f73b65f7049bf1ce4
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-02-04 14:19:25 -0800

    coded a quick method

--- js/sortingAlgo.js
+++ js/sortingAlgo.js
@@ -409,3 +409,20 @@ var merge = function(left, right) {
 ArrayList.prototype.quickSort = function() {
   quick(this.array, 0, this.array.length-1);
 };
+
+
+//  quick => helper function to quickSort
+var quick = function(array, left, right) {
+  var index;
+  
+  if(array.length > 1) {
+    index = partition(array, left, right);
+    
+    if(left < index-1) {
+      quick(array, left, index-1);
+    }
+    if(index < right) {
+      quick(array, index, right);
+    }
+  }
+};
