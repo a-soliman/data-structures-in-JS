0e924cd8294cb8b25c8bca1ebf48de03d068f24c
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-02-08 07:54:33 -0800

    coded the cons func

--- js/algoProb.js
+++ js/algoProb.js
@@ -1,1 +1,20 @@
 //1) Minimum Coin Change
+
+function CoinsCounter(coinsSet, amount) {
+  
+  // a recursive function to count the remained amount and re calculate it to the same coin.
+  var recount = function(remainedAmount) {
+    //only works if the ramind amount is greater than the same coin in the loop
+    if(remainedAmount >= coinsSet[i]) {
+      newAmount = amount - coinsSet[i];
+      coinUsed.push(coinsSet[i]);
+      amount= newAmount;
+      
+      // recalls it self to double check
+      recount(amount);
+    }
+    // a wayout.
+    else{
+      return;
+    }
+  };
