1e5a6486ab9397460d9e5f86d768b01506d154b5
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-02-09 08:04:28 -0800

    coded an add to head method

--- js/openMind.js
+++ js/openMind.js
@@ -23,3 +23,16 @@ function Node(value, next, prev) {
   this.next = next;
   this.prev = prev;
 }
+
+LinkedList.prototype.addToHead = function(value){
+  var newNode = new Node(value, this.head, null);
+  
+  if(this.head) {
+    this.head.prev = newNode;
+  }
+  else {
+    this.tail = newNode;
+  }
+  this.head = newNode;
+}
+
