3f560d4c981fafb450c290166ff405ecf2383153
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-02-09 08:05:10 -0800

    coded an add to tail method

--- js/openMind.js
+++ js/openMind.js
@@ -36,3 +36,16 @@ LinkedList.prototype.addToHead = function(value){
   this.head = newNode;
 }
 
+
+LinkedList.prototype.addToTail = function(value) {
+  var newNode = new Node(value, null, this.tail);
+  
+  if(this.tail) {
+    this.tail.next = newNode;
+  }
+  else {
+    this.head = newNode;
+  }
+  this.tail = newNode;
+};
+
