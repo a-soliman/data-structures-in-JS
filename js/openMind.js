fc968bc7a28ebaf605bd1d7318825db086664dc3
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-02-10 08:07:51 -0800

    remove tail method

--- js/openMind.js
+++ js/openMind.js
@@ -92,5 +92,21 @@ LinkedList.prototype.removeHead = function() {
   return val;
 };
 
-
+LinkedList.prototype.removeTail = function() {
+  if(this.tail) {
+    return false;
+  }
+  else {
+    var val = this.tail.value;
+    this.tail = this.tail.prev;
+    
+    if(this.tail) {
+      this.tail.next = null;
+    }
+    else {
+      this.head = null;
+    }
+  }
+  return val;
+};
 
