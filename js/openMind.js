da85bf046dae99f722045a9091d619fcc8da59c5
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-02-11 08:12:50 -0800

    coded a HashNode cons func

--- js/openMind.js
+++ js/openMind.js
@@ -175,4 +175,10 @@ function HashTable(size) {
   this.numBuckets = this.buckets.length;
 }
 
+function HashNode(key, value, next) {
+  this.key = key;
+  this.value = value;
+  this.next = next || null;
+}
+
 
