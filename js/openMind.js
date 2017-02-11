c9732b6f9813efd37e4ecf4ef8dd61c6c69dbbcc
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-02-11 08:13:53 -0800

    coded an insert method

--- js/openMind.js
+++ js/openMind.js
@@ -192,3 +192,26 @@ HashTable.prototype.hash = function(key) {
   return bucket;
 };
 
+HashTable.prototype.insert = function(key, value) {
+  var index = this.hash(key);
+  
+  if(!this.buckets[index]) {
+    this.buckets[index] = new HashNode(key, value);
+  }
+  else if(this.buckets[index].key === key) {
+    this.buckets[index].value = value;
+  }
+  else {
+    var currentNode = this.buckets[index];
+    
+    while(currentNode.next) {
+      if(currentNode.next.key === key) {
+        currentNode.next.value = value;
+        return
+      }
+      currentNode = currentNode.next
+    }
+    currentNode.next = new HashNode(key, value);
+  }
+}
+
