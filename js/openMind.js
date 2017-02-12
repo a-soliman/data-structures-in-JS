ac5e2479e94e32fa48a60a2911af2b15081d5a6a
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-02-12 08:17:02 -0800

    coded an insert method

--- js/openMind.js
+++ js/openMind.js
@@ -257,3 +257,22 @@ function BST(value) {
   this.left = null;
   this.right = null;
 }
+
+BST.prototype.insert = function(value) {
+  if(value <= this.value) {
+    if(!this.left) {
+      this.left = new BST(value);
+    }
+    else {
+      this.left.insert(value);
+    }
+  }
+  else if(value > this.value) {
+    if(!this.right) {
+      this.right = new BST(value);
+    }
+    else {
+      this.right.insert(value);
+    }
+  }
+};
